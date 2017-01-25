70243a2b7c757a1a2f67ac31d68a1073b02b521a
“aroncey” <artuhrroncey@gmail.comssh-keygen -t rsa -C arthurroncey@gmail.com>
2017-01-24 23:00:09 -0500

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,20 @@
+def my_collect(argument)
 
+	# i = 0
+	# while argument.length > i 
+	# 	argument.collect { |name| name.split(" ").first }
+	# 	i = i+1
+
+	# 	my_collect(array) do |name|
+ #  			name.split(" ").first
+	# 	end
+	# 	yield argument[i]
+	# # 	my_collect(collection) do |lang|
+ # #  			lang.uppercase
+ 	argument.collect do |item, i|
+ 		# item.split(" ").first
+ 		yield item
+	end
+end
+	# argument
 
